ab6d1520ef8e4582da9632796d83fae1a78a51c5
Joshua <josh01john@gmail.com>
2025-02-01 14:12:46 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 from flask import Flask, request, render_template
+from flask_pymongo import PyMongo
+import requests
+import pyttsx3
+
+
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://grhackru:dbgrhackRU@cluster.mongodb.net/todo_db"
+mongo = PyMongo(app)
+engine = pyttsx3.init()
 
-# Initialize the text-to-speech engine
-
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET','POST'])
 
 def index():
-    return render_template('index.html') 
+    if request.method == 'POST':
+        name = request.form['name']
+        r = requests.get(f'https://www.themealdb.com/api/json/v1/1/search.php?s={name}')
+        data = r.json()
+        if data.get("meals"):
+            mealnames = [meal["strMeal"] for meal in data["meals"]]
+        return "No meals found."
+    return render_template('index.html')
+    
 
 
 if __name__ == '__main__':
