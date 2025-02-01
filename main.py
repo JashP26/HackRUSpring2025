b7ff7f153d0a396776957337c93dd707bbc76c1c
JashP26 <81722532+JashP26@users.noreply.github.com>
2025-02-01 12:33:00 -0500

    updated main.py

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
 from flask import Flask, request, render_template
-import pyttsx3
-from deep_translator import GoogleTranslator
 
 app = Flask(__name__)
 
 # Initialize the text-to-speech engine
-engine = pyttsx3.init()
-
-def translate_text(string):
-    return GoogleTranslator(source="auto",target="es").translate(string)
 
 
-@app.route('/work', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 
 def index():
-    if request.method == 'POST':
-        name = request.form['name']
-        text = translate_text(name)
-        engine.say(text)  # Queue the text
-        engine.runAndWait()  # Speak the text
-        return f'{text}! '
     return render_template('index.html') 
 
 
